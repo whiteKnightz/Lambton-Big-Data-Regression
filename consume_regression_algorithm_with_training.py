dcf7b44d432a4ce465b53c80f1e9c7e55ea574f3
Abhishek Satyal <ab.satyal@gmail.com>
2022-04-18 15:39:45 +0545

    feat: add k-means notepad

--- consume_regression_algorithm_with_training.py
+++ consume_regression_algorithm_with_training.py
@@ -1,3 +1,7 @@
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import r2_score
+import statsmodels.api as sm
+from sklearn.linear_model import LinearRegression
 import matplotlib.pyplot as plt  # plotting
 import pandas as pd  # data processing, CSV file I/O (e.g. pd.read_csv)
 import statsmodels.api as sm
@@ -7,8 +11,6 @@ df = pd.read_csv('./data/measurements.csv')
 df['distance'] = df['distance'].str.replace(',', '.').astype(float)
 df['consume'] = df['consume'].str.replace(',', '.').astype(float)
 df['temp_inside'] = df['temp_inside'].str.replace(',', '.').astype(float)
-
-# df = pd.read_excel('./data/measurements2.xlsx',sheet_name='Sheet1')
 
 df.shape
 print(df.shape)
@@ -36,79 +38,21 @@ def regression_algorithm(X,y):
     X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=0.7,
                                                         test_size=0.3, random_state=100)
 
-
-
-
-
-    import statsmodels.api as sm
-
-    # Adding a constant to get an intercept
     X_train_sm = sm.add_constant(X_train)
 
-
-
-
-
-
-
-
     lr = sm.OLS(y_train, X_train_sm).fit()
-
-    # Printing the parameters
     lr.params
     print(lr.params)
-
-
-
-
-
-
-
 
     lr.summary()
     print(lr.summary())
 
-
-
-
-
-
-
-
     plt.scatter(X_train, y_train)
-    plt.plot(X_train, 4.9038 + -0.0033 * X_train, 'r')
+    plt.plot(X_train, 7.1164 + -0.1183 * X_train, 'r')
     plt.show()
 
-
-
-
-
-
-
-
-
-
-
-
     y_train_pred = lr.predict(X_train_sm)
-
-    # Creating residuals from the y_train data and predicted y_data
     res = (y_train - y_train_pred)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
     fig = plt.figure()
     sns.histplot(res, bins=15)
@@ -116,197 +60,41 @@ def regression_algorithm(X,y):
     plt.xlabel('y_train - y_train_pred', fontsize=15)
     plt.show()
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
     plt.scatter(X_train, res)
     plt.show()
 
-
-
-
-
-
-
-
-
-
-
-
     X_test_sm = sm.add_constant(X_test)
-
-    # Predicting the y values corresponding to X_test_sm
     y_test_pred = lr.predict(X_test_sm)
-
-    # Printing the first 15 predicted values
     y_test_pred
+    print(y_test_pred)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    from sklearn.metrics import r2_score
-
-    # Checking the R-squared value
     r_squared = r2_score(y_test, y_test_pred)
     r_squared
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    print(r_squared)
 
     plt.scatter(X_test, y_test)
     plt.plot(X_test, y_test_pred, 'r')
     plt.show()
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    from sklearn.model_selection import train_test_split
-    X_train_lm, X_test_lm, y_train_lm, y_test_lm = train_test_split(X, y, train_size=0.7,
-                                                                    test_size=0.3, random_state=100)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    X_train_lm, X_test_lm, y_train_lm, y_test_lm = train_test_split(X, y, train_size=0.7, test_size=0.3, random_state=100)
 
     X_train_lm.shape
-
-    # Adding additional column to the train and test data
     X_train_lm = X_train_lm.values.reshape(-1, 1)
     X_test_lm = X_test_lm.values.reshape(-1, 1)
-
     print(X_train_lm.shape)
     print(X_test_lm.shape)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    from sklearn.linear_model import LinearRegression
-
-    # Creating an object of Linear Regression
     lm = LinearRegression()
-
-    # Fit the model using .fit() method
     lm.fit(X_train_lm, y_train_lm)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
     print("Intercept :", lm.intercept_)
-
-    # Slope value
     print('Slope :', lm.coef_)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
     y_train_pred = lm.predict(X_train_lm)
     y_test_pred = lm.predict(X_test_lm)
+    print(f'R2 score of Y train: {r2_score(y_train, y_train_pred)}')
+    print(f'R2 score of Y test: {r2_score(y_test, y_test_pred)}')
 
-    # Comparing the r2 value of both train and test data
-    print(r2_score(y_train, y_train_pred))
-    print(r2_score(y_test, y_test_pred))
 
-dfGasType1 = df.query('gas_type=="SP98"')
-X = dfGasType1['distance']
-y = dfGasType1['consume']
-regression_algorithm(X,y)
-
-dfGasType2 = df.query('gas_type=="E10"')
-X = dfGasType2['distance']
-y = dfGasType2['consume']
-regression_algorithm(X,y)
+dfSpeed30 = df.query('speed<30')
+X = dfSpeed30['distance']
+y = dfSpeed30['consume']
+regression_algorithm(X, y)
